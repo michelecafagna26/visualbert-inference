0cd91f1676077ee8293a296f6b44ba9eb2f85241
Michele Cafagna <m.cafagna26@gmail.com>
2021-01-13 20:07:45 +0100

    fixed the inference loop + added attention extraction and saving

--- inference.py
+++ inference.py
@@ -229,215 +229,45 @@ stop_epoch = args.num_train_epochs
 
 save_every = args.get("save_every", None)
 
+try:
+    ### This is the inference part
+    val_probs = []
+    val_labels = []
+    val_size = 0.0
+    val_loss_sum = 0.0
 
-#for epoch_num in range(start_epoch, stop_epoch):
-for i in range(1):
-    """
-    train_results = []
-    norms = []
-    train_model.model.train()
-    if not args.get("skip_training", False):
-        for b, (time_per_batch, batch) in enumerate(time_batch(tqdm(train_loader), reset_every=ARGS_RESET_EVERY)):
+    val_acc = 0.0
+    val_acc_upper = 0.0
+    val_instance_counter = 0.0
 
+    val_next_sentence_loss_sum = 0.0
+
+    train_model.eval()
+
+    val_counter = 0
+
+    for i, batch in enumerate(val_loader):
+        with torch.no_grad():
+
+            tokens = [val_loader.dataset.tokenizer.ids_to_tokens[i] for i in batch['bert_input_ids'][0].tolist()]
             batch = _to_gpu(batch)
-            
-            output_dict = train_model.step(batch)
+            output_dict = train_model.step(batch, eval_mode = True)
+            print(output_dict.keys())
 
-            num_batches += 1
+            index = val_loader.dataset.items[i]['image_id']
+            outfile_name = "COCO_val2014_000000{}.out.npz".format(index)
 
-            train_results.append(pd.Series({'loss': output_dict['loss'].mean().item(),
-                                            'crl': output_dict.get("cnn_regularization_loss", 0.0),
-                                            'next_sentence_loss': output_dict["next_sentence_loss"].mean().item() if "next_sentence_loss" in output_dict else 0.0,
-                                            'masked_lm_loss': output_dict["masked_lm_loss"].mean().item() if "masked_lm_loss" in output_dict else 0.0,
-                                            'accuracy': (train_model.model.module).get_metrics(
-                                                reset=(b % ARGS_RESET_EVERY) == 0)[
-                                                'accuracy'],
-                                            'sec_per_batch': time_per_batch,
-                                            'hr_per_epoch': len(train_loader) * time_per_batch / 3600,
-                                            }))
-            if b % ARGS_RESET_EVERY == 0 and b > 0:
-                print("e{:2d}b{:5d}/{:5d}. \nsumm:\n{}\n~~~~~~~~~~~~~~~~~~\n".format(
-                    epoch_num, b, len(train_loader),
-                    pd.DataFrame(train_results[-ARGS_RESET_EVERY:]).mean(),
-                ), flush=True)
+            output_dict['attention_weights'] = [ m.cpu().detach().numpy() for m in output_dict['attention_weights']]
 
-            if save_every is not None and b % save_every == 0 and b != 0:
-                train_model.save_checkpoint_step(args.folder, b, epoch_num)
+            np.savez("visualbert-inference/out_attn/" + outfile_name, tokens = tokens, attention_weights = output_dict['attention_weights'] )
 
-        print("---\nTRAIN EPOCH {:2d}:\n{}\n----".format(epoch_num, pd.DataFrame(train_results).mean()))
+except KeyboardInterrupt:
 
-    """
-    try:
-        ### This is the eval part
-        val_probs = []
-        val_labels = []
-        val_size = 0.0
-        val_loss_sum = 0.0
-
-        val_acc = 0.0
-        val_acc_upper = 0.0
-        val_instance_counter = 0.0
-
-        val_next_sentence_loss_sum = 0.0
-
-        train_model.eval()
-
-        val_counter = 0
-
-        ############ Different reporting parameters
-
-        # for vqa, nlvr, flickr
-        do_test = args.get("do_test", False) ## This one is for vqa
-        if do_test:
-            val_loader = test_loader
-            val_dataset = val_loader.dataset
-        vcr_save_result = args.get("vcr_save_result", False) # This one is for vcr
-
-        for b, (time_per_batch, (batch, tokens)) in enumerate(time_batch(val_loader if args.no_tqdm else tqdm(val_loader), reset_every=ARGS_RESET_EVERY)):
-            with torch.no_grad():
-                batch = _to_gpu(batch)
-                output_dict = train_model.step(batch, eval_mode = True)
-                print(output_dict.keys())
-
-                index = val_loader.dataset.items[b]['image_id']
-                outfile_name = "COCO_val2014_000000{}.out.npz".format(index)
-
-                output_dict['tokens'] = tokens
-                output_dict['attention_weights'] = [ m.cpu().detach().numpy() for m in output_dict['attention_weights']]
-
-                np.savez("XX/X_features/attw/" + outfile_name, tokens = output_dict['tokens'], attention_weights = output_dict['attention_weights'] )
-
-
-
-
-                """
-                if not args.pretraining:
-                    # Pretty clumsy code
-                    if args.model.training_head_type == "vqa":
-                        val_probs.append(output_dict['logits'].detach().cpu())
-                        if not do_test:
-                            val_labels.append(batch['label'].detach().cpu())
-                    elif args.model.training_head_type == "flickr":
-                        # This is because of multi-GPU
-                        val_acc += (output_dict["accuracy"] * output_dict["entity_num"].float()).sum(-1).item()
-                        val_acc_upper += (output_dict["upperbound_accuracy"] * output_dict["entity_num"].float()).sum(-1).item()
-                        val_instance_counter += output_dict["entity_num"].sum(-1).item()
-
-                    elif args.model.training_head_type == "multichoice":
-                        val_probs.append(output_dict['logits'].detach().cpu().numpy())
-                        if not do_test:
-                            val_labels.append(batch['label'].detach().cpu().numpy())
-                    elif args.model.training_head_type == "nlvr":
-                        val_probs.append(output_dict['logits'].detach().cpu().numpy())
-                        val_labels.append(batch['label'].detach().cpu().numpy())
-
-                else:
-                    val_labels.append(batch['label'].detach().cpu().numpy())
-
-                if not do_test:
-                    val_loss_sum += output_dict['loss'].mean().item() * batch['label'].size(0)
-                    val_counter += batch['label'].size(0)
-
-                    if "next_sentence_loss" in output_dict:
-                        val_next_sentence_loss_sum += output_dict['next_sentence_loss'].mean().item() * batch['label'].size(0)
-            """
-            break
-        """
-        if not args.pretraining:
-            if args.model.training_head_type == "vqa":
-                if do_test:
-                    val_probs = np.concatenate(val_probs, 0)
-                    val_probs = torch.Tensor(val_probs)
-                    val_probs = val_probs.squeeze(1)
-                    val_dataset.generate_test_file(val_probs, os.path.join(args.folder, "result.json"))
-                    print("Finished testing")
-                    assert(0)
-                else:
-                    val_labels = np.concatenate(val_labels, 0)
-                    val_probs = np.concatenate(val_probs, 0)
-                    val_probs = torch.Tensor(val_probs)
-                    val_labels = torch.Tensor(val_labels)
-                    val_probs = val_probs.squeeze(1)
-                    acc = torch.sum(compute_score_with_logits(val_probs, val_labels)) / val_labels.size(0)
-                    acc = acc.squeeze(-1).item()
-            elif args.model.training_head_type == "flickr":
-                acc = val_acc / val_instance_counter
-                val_acc_upper = val_acc_upper / val_instance_counter
-                print("Upper bound: {:.5f}".format(val_acc_upper))
-            elif args.model.training_head_type == "multichoice": #VCR
-                if not do_test:
-                    val_labels = np.concatenate(val_labels, 0)
-                val_probs = np.concatenate(val_probs, 0)
-                if vcr_save_result:
-                    if do_test:
-                        file_name = "test"
-                    else:
-                        file_name = "val"
-
-                    save_file_name = os.path.join(args.folder, file_name + "_qa.np")
-                    if args.rationale:
-                        save_file_name = os.path.join(args.folder, file_name + "_qar.np")
-                    if do_test:
-                        np.save(save_file_name, val_probs)
-                    else:
-                        np.savez(save_file_name+'z', val_probs=val_probs, val_labels=val_labels)
-
-                        #np.save(save_file_name, (val_probs, val_labels))
-                    print("Saved result to {}".format(save_file_name))
-                    assert(0)
-
-                acc = float(np.mean(val_labels == val_probs.argmax(1)))
-            elif args.model.training_head_type == "nlvr":
-                val_labels = np.concatenate(val_labels, 0)
-                val_probs = np.concatenate(val_probs, 0)
-                if args.get("report", False):
-                    val_probs = val_probs.argmax(1)
-                    assert(val_probs.shape[0]) == len(val_dataset)
-                    result = []
-                    for index, i in enumerate(val_dataset.items):
-                        label = "True" if val_probs[index] == 1 else "False"
-                        result.append(i["identifier"] + "," + label)
-                    with open(os.path.join(args.folder, "results.csv"), "w") as f:
-                        f.write("\n".join(result))
-                    assert(0)
-                acc = float(np.mean(val_labels == val_probs.argmax(1)))
-            if not do_test:
-                val_loss_avg = val_loss_sum / val_counter
-                print("Val epoch {} has acc {:.5f} and loss {:.5f}".format(epoch_num, acc, val_loss_avg), flush=True)
-            else:
-                print("Val epoch {} has acc {:.5f}".format(epoch_num, acc), flush=True)
-                assert(0)
-            val_metric_per_epoch.append(acc)
-        else:
-            val_loss_avg = val_loss_sum / val_counter
-            val_next_sentence_loss_avg = val_next_sentence_loss_sum / val_counter
-            print("Val epoch {} has loss {:.5f}, next sentence loss {:.5f}".format(epoch_num, val_loss_avg, val_next_sentence_loss_avg), flush=True)
-            val_metric_per_epoch.append(-val_loss_avg)
-        
-        if int(np.argmax(val_metric_per_epoch)) < (len(val_metric_per_epoch) - 1 - args.patience):
-            print("Stopping at epoch {:2d}".format(epoch_num))
-            break
-
-        """
-        ############### Save model
-        """
-        if not args.get("skip_training", False):
-            train_model.save_checkpoint(args.folder, epoch_num, val_metric_per_epoch, is_best=int(np.argmax(val_metric_per_epoch)) == (len(val_metric_per_epoch) - 1))
-        """
-    except KeyboardInterrupt:
-        """
-        if not args.get("skip_training", False):
-            train_model.save_checkpoint(args.folder, epoch_num, None, is_best=False)
-            """
-        print("Something Went Wrong with Evaluation. Stopped.")
+    print("Something Went Wrong with Evaluation. Stopped.")
+    assert(0)
+except Exception as e:
+    print(e)
+    print("Something Went Wrong with Evaluation. Ignored.")
+    if args.get("skip_training", False):
         assert(0)
-    except Exception as e:
-        print(e)
-        """
-        if not args.get("skip_training", False):
-            train_model.save_checkpoint(args.folder, epoch_num, None, is_best=False)
-            """
-        print("Something Went Wrong with Evaluation. Ignored.")
-        if args.get("skip_training", False):
-            assert(0)
         
